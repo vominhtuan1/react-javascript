6e810dc2bd49224329908d8ff19426b6d10fa3c3
vominhtuan1 <19522487@gm.uit.edu.vn>
2023-03-24 16:55:42 +0700

    feat: initial project

---
+++ src/components/HomePage.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+export default function HomePage(props) {
+	const [age, setAge] = useState(10);
+
+	const changeAge = () => {
+		setAge("ten");
+	};
+
+	const handleOnChange = (event) => {
+		console.log(event.target.vale);
+	};
+
+	const fetchData = async () => {
+		const res = await axios.get("https://official-joke-api.appspot.com/random_joke");
+		const data = res.data;
+		console.log(data);
+	};
+
+	useEffect(() => {
+		changeAge();
+		fetchData();
+	}, []);
+
+	return (
+		<div>
+			<h3>HomePage</h3>
+			<p>{props.name}</p>
+			<p>{age / 10}</p>
+			<input type="text" onChange={handleOnChange} />
+		</div>
+	);
+}
